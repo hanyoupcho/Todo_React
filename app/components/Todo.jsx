b3eee28ff3c2f231093b6b3271a78f342589b20c
hanyoupcho <hanyoup.cho@gmail.com>
2017-04-19 11:57:45 +0900

    115

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -5,23 +5,26 @@ import moment from 'moment';
 class Todo extends React.Component {
   render() {
     var {id, text, completed, createdAt, completedAt} = this.props;
+    var todoClassName = completed ? "todo todo-completed" : "todo";
     var renderDate = () => {
       var message = 'Created ';
       var timestamp = createdAt;
 
       if (completed) {
-        message = 'completed ';
+        message = 'Completed ';
         timestamp = completedAt;
       }
       return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
     };
     return (
-      <div onClick={() => {
-          this.props.onToggle(id);
-        }}>
-        <input name="completed" type="checkbox" checked={completed} readOnly/>
-        <p>{text}</p>
-        <p>{renderDate()}</p>
+      <div className={todoClassName} onClick={() => {this.props.onToggle(id);}}>
+        <div>
+          <input name="completed" type="checkbox" checked={completed} readOnly/>
+        </div>
+        <div>
+          <p>{text}</p>
+          <p className="todo__subtext">{renderDate()}</p>
+        </div>
       </div>
     );
   }
--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -5,13 +5,21 @@ import Todo from 'Todo';
 class TodoList extends React.Component {
   render() {
     var {todos} = this.props;
+
     var renderTodos = () => {
+      if (todos.length === 0) {
+        return (
+          <p className="container__message">Nothing To Do</p>
+        );
+      }
+
       return todos.map((todo) => {
         return (
           <Todo key={todo.id} {...todo} onToggle={this.props.onToggle}/>
         );
       });
     };
+
     return (
       <div>
         {renderTodos()}
